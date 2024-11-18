a626f9ddaa54cd308fbd0e0f383fda319c75ea99
Rodri541 <rodridiazmosegue4@gmail.com>
2024-11-17 23:23:27 -0300

    Mod8

--- Proyecto/ApiProject/src/index.js
+++ Proyecto/ApiProject/src/index.js
@@ -3,14 +3,27 @@ import { PORT } from './config.js';
 import productRoutes from './routes/products.routes.js';
 import userRoutes from './routes/users.routes.js';
 import categoriesRoutes from './routes/categories.routes.js';
-import suppliersRoutes from './routes/suppliers.routes.js'
+import suppliersRoutes from './routes/suppliers.routes.js';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://jolly-coast-05026500f.5.azurestaticapps.net',  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));  
 app.use(express.json());
 
-app.use(productRoutes, userRoutes, categoriesRoutes, suppliersRoutes);
+// Usar las rutas
+app.use(productRoutes);
+app.use(userRoutes);
+app.use(categoriesRoutes);
+app.use(suppliersRoutes);
 
-app.listen(PORT);
-console.log('servidor iniciado... Puerto: ', PORT);
+app.listen(PORT, () => {
+  console.log('Servidor iniciado... Puerto: ', PORT);
+});
