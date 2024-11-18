d2d6fdda02a85b6fddfe1b124bffa64af6eb17e8
Rodri541 <rodridiazmosegue4@gmail.com>
2024-11-17 23:25:09 -0300

    Mod9

--- Proyecto/React_E-Commerce-main/src/config.js
+++ Proyecto/React_E-Commerce-main/src/config.js
@@ -1,4 +1,4 @@
 // React_E-Commerce-main/src/config.js
-const API_URL = "http://localhost:3000"; // Cambia el puerto si tu API usa otro
+const API_URL = "lacandelaapi2-dnewg3f7asd5fsem.canadacentral-01.azurewebsites.net"; // Cambia el puerto si tu API usa otro
 
 export default API_URL;
