83a993923425074823103e08517bcde88a804192
Rodri541 <rodridiazmosegue4@gmail.com>
2024-11-18 18:15:25 -0300

    Mod12

--- Proyecto/React_E-Commerce-main/src/config.js
+++ Proyecto/React_E-Commerce-main/src/config.js
@@ -1,4 +1,4 @@
 // React_E-Commerce-main/src/config.js
-const API_URL = "https://jolly-coast-05026500f.5.azurestaticapps.net"; // Cambia el puerto si tu API usa otro
+const API_URL = "lacandelaapi2-dnewg3f7asd5fsem.canadacentral-01.azurewebsites.net"; // Cambia el puerto si tu API usa otro
 
 export default API_URL;
